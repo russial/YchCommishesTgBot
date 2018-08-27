6bcdd9830803b0f0a7d4751fa7ca6f93a601afd9
Nikolay Yanchuk <russial2011@gmail.com>
2018-08-27 23:57:04 +0300

    PEP-8 sqlite module

--- sqlitedb.py
+++ sqlitedb.py
@@ -2,20 +2,22 @@ import sqlite3
 
 class YchDb:
     # Queries
-    __create_tables_query="""
-    CREATE TABLE IF NOT EXISTS ychs
-    (chatid bigint, ychid int, maxprice float, endtime bigint, link varchar,
-    PRIMARY KEY (chatid, ychid))
-    """
-    __add_new_query='INSERT OR REPLACE INTO ychs (chatid, ychid, maxprice, endtime, link) VALUES (?,?,?,?,?)'
-    __select_all_query='SELECT * FROM ychs'
-    __select_all_by_user_query='SELECT (ychid) FROM ychs WHERE chatid=?'
-    __delete_query='DELETE FROM ychs WHERE ychid = ?'
+    __create_tables_query = (
+        'CREATE TABLE IF NOT EXISTS ychs (chatid bigint,ychid int,'
+        'maxprice float,endtime bigint,link varchar,'
+        'PRIMARY KEY (chatid, ychid))'
+    )
+    __add_new_query = (
+        'INSERT OR REPLACE INTO ychs (chatid,ychid,maxprice,endtime,link) '
+        'VALUES (?,?,?,?,?)'
+    )
+    __select_all_query = 'SELECT * FROM ychs'
+    __select_all_by_user_query = 'SELECT (ychid) FROM ychs WHERE chatid=?'
+    __delete_query = 'DELETE FROM ychs WHERE ychid = ?'
 
     def __init__(self, path):
         self.path = path
         self.conn = sqlite3.connect(path,check_same_thread=False)
-        # TODO: Find out, if there's need to check_same_thread=False directive
         self.__init_db()
 
     def __init_db(self):
