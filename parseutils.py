11c223f238ab6702ab728a16a9c19f9ab3bd587b
Nikolay Yanchuk <russial2011@gmail.com>
2018-09-08 10:40:52 +0300

    Ref

--- parseutils.py
+++ parseutils.py
@@ -12,25 +12,20 @@ headers = {
 
 ych_url = 'https://ych.commishes.com/auction/getbids/{}'
 
-def get_ychid_by_link(url):
+def get_ychid_by_link(url: str) -> int:
     url = url.split('/')
     if len(url) >= 6:
         return baseutils.get_10(url[5])
-    else:
-        return 0
+    return 0
 
-def get_ych_info(id):
+def get_ych_info(id: int) -> list:
     while True:
         try:
             ych_json = requests.get(ych_url.format(id), headers)
             data = ych_json.json()
             break
-        except ConnectionError:
-            print("Except ConnectionError")
-            time.sleep(2)
-            print("Slept for 2 seconds, trying again")
-        except ValueError:
-            print("Except JSONDecodeError")
+        except (ConnectionError, ValueError) as e:
+            print("Except " + str(e))
             time.sleep(2)
             print("Slept for 2 seconds, trying again")
     data['id'] = id
